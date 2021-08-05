8f162743551c755512ac108083ab36781ef829b7
Alla Kobyzeva <apkobyzeva@yandex.ru>
2021-08-05 23:36:42 +0300

    Тестирование валидации

--- src/test/java/DebitCardApplicationTest.java
+++ src/test/java/DebitCardApplicationTest.java
@@ -47,4 +47,45 @@ class DebitCardApplicationTest {
         String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+    @Test
+    void shouldTestName() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("gfgfhfhf");
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input__sub")).getText();
+        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        assertEquals(expectedText, actualText);
+    }
+    @Test
+    void shouldTestNoName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input__sub")).getText();
+        String expectedText = "Поле обязательно для заполнения";
+        assertEquals(expectedText, actualText);
+    }
+    @Test
+    void shouldTestPhoneNumber() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иванов Василий");
+        elements.get(1).sendKeys("9270000000");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> elements2 = driver.findElements(By.className("input__sub"));
+        String actualText = elements2.get(1).getText();
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        assertEquals(expectedText, actualText);
+    }
+    @Test
+    void shouldTestNoCheckboxClick() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иванов Василий");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input_invalid")).getText();
+        String expectedText = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        assertEquals(expectedText, actualText);
+    }
 }
